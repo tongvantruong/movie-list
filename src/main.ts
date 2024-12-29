0b95a02da9b1b99bd7ef761e8f7d936f8d50dec5
tongvantruong <tong.tvt.truong@gmail.com>
2024-12-29 22:38:07 +0700

    handle global error, shhow UI

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, type ComponentPublicInstance } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import '@mdi/font/css/materialdesignicons.css'
 import '@/assets/scss/app.scss'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { globalErrorStore } from '@/stores/global-error'
+
+const { showError } = globalErrorStore()
 
 const vuetify = createVuetify({
   theme: {
@@ -16,5 +19,10 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
+
+app.config.errorHandler = (error: unknown, _: ComponentPublicInstance | null, info: string) => {
+  showError()
+  console.log(error, info)
+}
 
 app.mount('#app')
